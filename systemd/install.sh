67ebad7e5bf287b82ad4c194543349cec4ebc279
Emmanuel ROECKER <emmanuel.roecker@glicer.com>
2017-05-20 19:03:33 +0000

    update install

--- systemd/install.sh
+++ systemd/install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+service gltodo stop
+rm /etc/systemd/system/gltodo.service
 cp gltodo.service /etc/systemd/system
+rm /etc/systemd/system/multi-user.target.wants/gltodo.service
 cd /etc/systemd/system/multi-user.target.wants
 ln -s ../gltodo.service .
+systemctl daemon-reload
+service gltodo start
 
